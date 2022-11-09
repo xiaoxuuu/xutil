a81ab047ddb29e60a7293072094b235484d4c3fe
小徐 <xuhaoran@live.com>
2022-11-09 17:21:40 +0800

    新增数学计算

---
+++ src/main/java/live/xiaoxu/util/XString.java
@@ -0,0 +1,22 @@
+package live.xiaoxu.util;
+
+/**
+ * 字符串工具类
+ * <p>
+ * 2022/11/9 15:04
+ *
+ * @author XiaoXu
+ */
+public class XString {
+
+    /**
+     * 判断字符串是否为空
+     *
+     * @param str 参数
+     * @return 字符串返回 true
+     */
+    public static boolean isBlank(String str) {
+
+        return null == str || str.trim().length() == 0;
+    }
+}

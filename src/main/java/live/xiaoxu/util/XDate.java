71655f701bf769633efb78090b6b7c93c307abe4
小徐 <xuhaoran@live.com>
2022-12-05 17:25:32 +0800

    新增日期类

---
+++ src/main/java/live/xiaoxu/util/XDate.java
@@ -0,0 +1,19 @@
+package live.xiaoxu.util;
+
+/**
+ * 日期工具类
+ * <p>
+ * 2022/11/29 11:45
+ *
+ * @author 小徐
+ */
+public class XDate {
+
+    /**
+     * 禁止实例化
+     */
+    private XDate() {
+    }
+
+    // TODO 待实现
+}

1d9cc2abf31af6a6ad2e6108decc3f4c05021b64
小徐 <x@xiaoxu.live>
2023-01-04 14:46:19 +0800

    新增工具类

---
+++ src/main/java/live/xiaoxu/constants/DateConstants.java
@@ -0,0 +1,32 @@
+package live.xiaoxu.constants;
+
+/**
+ * <p>日期静态变量</p>
+ *
+ * @author 小徐
+ * @since 2023/1/3 16:44
+ */
+public class DateConstants {
+
+    public static final String DEFAULT_DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
+
+    public static final String DATE_TIME_FORMAT_2 = "yyyy/MM/dd HH:mm:ss";
+
+    public static final String DATE_FORMAT = "yyyy-MM-dd";
+
+    public static final String DATE_FORMAT_2 = "yyyy/MM/dd";
+
+    public static final String MONTH_FORMAT = "yyyy-MM";
+
+    public static final String MONTH_FORMAT_2 = "yyyy/MM";
+
+    public static final String DATE_MARK = "yyyyMMdd";
+
+    public static final String YEAR = "yyyy";
+
+    public static final String WEEK = "E";
+
+    public static final String DEFAULT_TIME_FORMAT = "HH:mm:ss";
+
+    public static final String HOUR_MINUTE = "HH:mm";
+}

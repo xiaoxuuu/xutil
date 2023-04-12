fc8bc996c22410c8cfb0731170e5064630df3355
小徐 <xuhaoran@live.com>
2023-04-12 12:00:48 +0800

    新增方法

--- src/main/java/live/xiaoxu/util/XChineseUtils.java
+++ src/main/java/live/xiaoxu/util/XChineseUtils.java
@@ -10,6 +10,53 @@ import java.io.UnsupportedEncodingException;
  */
 public final class XChineseUtils {
 
+    private final static int[] LI_SEC_POS_VALUE = {1601, 1637, 1833, 2078, 2274,
+            2302, 2433, 2594, 2787, 3106, 3212, 3472, 3635, 3722, 3730, 3858,
+            4027, 4086, 4390, 4558, 4684, 4925, 5249, 5590};
+    private final static String[] LC_FIRST_LETTER = {"a", "b", "c", "d", "e",
+            "f", "g", "h", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
+            "t", "w", "x", "y", "z"};
+
+    /**
+     * 取得给定汉字的首字母,即声母
+     *
+     * @param chinese 给定的汉字
+     * @return 给定汉字的声母
+     */
+    public static String getFirstLetter(String chinese) {
+
+        if (chinese == null || chinese.trim().length() == 0) {
+            return "";
+        }
+        chinese = conversionStr(chinese, "GB2312", "ISO8859-1");
+
+        // 判断是不是汉字
+        if (chinese.length() > 1) {
+            // 汉字区码
+            int liSectorCode = chinese.charAt(0);
+            // 汉字位码
+            int liPositionCode = chinese.charAt(1);
+            liSectorCode = liSectorCode - 160;
+            liPositionCode = liPositionCode - 160;
+            // 汉字区位码
+            int liSecPosCode = liSectorCode * 100 + liPositionCode;
+            if (liSecPosCode > 1600 && liSecPosCode < 5590) {
+                for (int i = 0; i < 23; i++) {
+                    if (liSecPosCode >= LI_SEC_POS_VALUE[i]
+                            && liSecPosCode < LI_SEC_POS_VALUE[i + 1]) {
+                        chinese = LC_FIRST_LETTER[i];
+                        break;
+                    }
+                }
+            } else {
+                // 非汉字字符,如图形符号或ASCII码
+                chinese = conversionStr(chinese, "ISO8859-1", "GB2312");
+                chinese = chinese.substring(0, 1);
+            }
+        }
+        return chinese;
+    }
+
     /**
      * 字符串编码转换
      *

c6728825c1cf0308d6a315679f0fdf90cd512b5e
小徐 <xuhaoran@live.com>
2023-05-06 14:12:34 +0800

    测试方法调整

--- src/test/java/live/xiaoxu/util/random/XRandomDateTest.java
+++ src/test/java/live/xiaoxu/util/random/XRandomDateTest.java
@@ -13,13 +13,13 @@ class XRandomDateTest {
     void get() {
 
         Set<String> set = Set.of("2000-01-01 00:00:00", "2000-01-01 00:00:01");
-        XRandomDate xRandomDate = XRandomDate.start()
+        XRandomDate xRandomDate = XRandomDate.init()
                 .setLeft(XDateUtils.cast("2000-01-01 00:00:00"))
                 .setRight(XDateUtils.cast("2000-01-01 00:00:01"));
         for (int i = 0; i < 10; i++) {
             Assertions.assertTrue(set.contains(XDateUtils.localDateTimeToString(xRandomDate.get())));
         }
-        LocalDateTime localDateTime = XRandomDate.start()
+        LocalDateTime localDateTime = XRandomDate.init()
                 .setLeft(XDateUtils.cast("2000-01-01 00:00:00"))
                 .setRight(XDateUtils.cast("2000-01-01 00:00:00"))
                 .get();

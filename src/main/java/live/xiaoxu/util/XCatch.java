d0e393b5d289674a5a9a07a22c4f63fb7784e3a3
小徐 <xuhaoran@live.com>
2023-06-29 14:18:27 +0800

    注释

--- src/main/java/live/xiaoxu/util/XCatch.java
+++ src/main/java/live/xiaoxu/util/XCatch.java
@@ -3,24 +3,49 @@ package live.xiaoxu.util;
 import java.util.Objects;
 import java.util.function.Supplier;
 
+/**
+ * <p>异常捕获</p>
+ *
+ * @author 小徐
+ * @since 2023/6/29 14:16
+ */
 public class XCatch<T> {
 
+    /**
+     * 消费者
+     */
     private final Supplier<T> supplier;
+
+    /**
+     * 禁止空参实例化
+     */
+    private XCatch() {
+        throw new IllegalAccessError(this.getClass().getName());
+    }
 
     private XCatch(Supplier<T> supplier) {
         this.supplier = supplier;
     }
 
-    private XCatch() {
-        throw new IllegalArgumentException("错误的构造请求");
-    }
-
+    /**
+     * 构造，传递待捕获代码段
+     *
+     * @param supplier 待捕获代码段
+     * @param <T>      代码执行结果类型
+     * @return 代码执行结果
+     */
     public static <T> XCatch<T> of(Supplier<T> supplier) {
 
         Objects.requireNonNull(supplier);
         return new XCatch<>(supplier);
     }
 
+    /**
+     * 执行代码，如果出现异常，执行 supplierException
+     *
+     * @param supplierException 出现异常后的处理代码
+     * @return 结果
+     */
     public T handle(Supplier<T> supplierException) {
         try {
             return supplier.get();

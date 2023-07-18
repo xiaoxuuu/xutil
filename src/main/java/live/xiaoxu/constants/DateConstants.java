86b7e2c16e73ff858b5ef4a7898430e2e0b559d3
小徐 <xuhaoran@live.com>
2023-07-18 14:28:29 +0800

    添加注释

--- src/main/java/live/xiaoxu/constants/DateConstants.java
+++ src/main/java/live/xiaoxu/constants/DateConstants.java
@@ -8,27 +8,63 @@ package live.xiaoxu.constants;
  */
 public class DateConstants {
 
+    /**
+     * 年月日时分秒
+     */
     public static final String DEFAULT_DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
 
+    /**
+     * 年月日时分秒
+     */
     public static final String DATE_TIME_FORMAT = "yyyy/MM/dd HH:mm:ss";
 
+    /**
+     * 年月日时分秒毫秒
+     */
     public static final String DATE_TIME_FORMAT_DETAIL = "yyyy-MM-dd HH:mm:ss.SSS";
 
+    /**
+     * 年月日
+     */
     public static final String DATE_FORMAT = "yyyy-MM-dd";
 
+    /**
+     * 年月日
+     */
     public static final String DATE_FORMAT_2 = "yyyy/MM/dd";
 
+    /**
+     * 年月
+     */
     public static final String YEAR_MONTH_FORMAT = "yyyy-MM";
 
+    /**
+     * 年月
+     */
     public static final String YEAR_MONTH_FORMAT_2 = "yyyy/MM";
 
+    /**
+     * 年月日
+     */
     public static final String DATE_MARK = "yyyyMMdd";
 
+    /**
+     * 年
+     */
     public static final String YEAR = "yyyy";
 
+    /**
+     * 周
+     */
     public static final String WEEK = "E";
 
+    /**
+     * 时分秒
+     */
     public static final String DEFAULT_TIME_FORMAT = "HH:mm:ss";
 
+    /**
+     * 时分
+     */
     public static final String HOUR_MINUTE = "HH:mm";
 }

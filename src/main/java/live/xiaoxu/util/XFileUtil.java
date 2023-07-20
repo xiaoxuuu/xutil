b6ccbb30da54cd07a1e159ef52d1f46306c7b941
小徐 <xuhaoran@live.com>
2023-07-20 14:01:38 +0800

    文件工具类

---
+++ src/main/java/live/xiaoxu/util/XFileUtil.java
@@ -0,0 +1,17 @@
+package live.xiaoxu.util;
+
+/**
+ * <p>文件工具类</p>
+ *
+ * @author 小徐
+ * @since 2023/7/20 13:59
+ */
+public class XFileUtil {
+
+    /**
+     * 禁止实例化
+     */
+    private XFileUtil() {
+        throw new IllegalAccessError(this.getClass().getName());
+    }
+}

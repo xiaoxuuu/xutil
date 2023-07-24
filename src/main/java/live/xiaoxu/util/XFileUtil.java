b8da1a62063b465e88557be9619c321f3f5452de
小徐 <xuhaoran@live.com>
2023-07-24 14:02:47 +0800

    新增方法

--- src/main/java/live/xiaoxu/util/XFileUtil.java
+++ src/main/java/live/xiaoxu/util/XFileUtil.java
@@ -1,5 +1,7 @@
 package live.xiaoxu.util;
 
+import java.io.*;
+
 /**
  * <p>文件工具类</p>
  *
@@ -13,5 +15,17 @@ public class XFileUtil {
      */
     private XFileUtil() {
         throw new IllegalAccessError(this.getClass().getName());
+    }
+
+    public static void inputStreamToFile(InputStream ins, File file) throws IOException {
+
+        OutputStream os = new FileOutputStream(file);
+        int bytesRead;
+        byte[] buffer = new byte[1024];
+        while ((bytesRead = ins.read(buffer)) != -1) {
+            os.write(buffer, 0, bytesRead);
+        }
+        os.close();
+        ins.close();
     }
 }

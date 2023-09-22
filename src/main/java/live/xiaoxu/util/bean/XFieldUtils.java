3a7258598922319f0a09cbc41509db6900dd9b4c
小徐 <xuhaoran@live.com>
2023-09-22 17:16:54 +0800

    添加工具类

---
+++ src/main/java/live/xiaoxu/util/bean/XFieldUtils.java
@@ -0,0 +1,17 @@
+package live.xiaoxu.util.bean;
+
+/**
+ * <p>字段工具类</p>
+ *
+ * @author 小徐
+ * @since 2023/9/22 17:16
+ */
+public class XFieldUtils {
+
+    /**
+     * 禁止实例化
+     */
+    private XFieldUtils() {
+        throw new IllegalAccessError(this.getClass().getName());
+    }
+}
